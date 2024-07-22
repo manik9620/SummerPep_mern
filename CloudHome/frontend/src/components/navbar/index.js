9b2ead4302a0a6a191cb6ef169b328303f1aee5b
Manik <manikproch9858@gmail.com>
2024-07-22 12:35:21 +0530

    UI updated as icon

--- CloudHome/frontend/src/components/navbar/index.js
+++ CloudHome/frontend/src/components/navbar/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import { useDispatch } from "react-redux";
 import { appLogout } from "../../store/slices/authSlice";
+import navicon from "../../public/assests/cloud-computing.png"
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -12,6 +13,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <div className="navbar-left-item">
+        <img src={navicon} alt="navicon" className="navicon" />
         <h2>Cloud Home</h2>
       </div>
       <div className="navbar-left-item">
